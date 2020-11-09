517c47b84155ba747a830025ff966edbcf8b1a34
Roman Dobrodii <roman.dobrodii@7sensingsoftware.com>
2020-11-09 16:10:38 +0100

    Implement modem class based on QSerialport it builds on Linux now!

--- modem_lin.h
+++ modem_lin.h
@@ -19,239 +19,241 @@
 #ifndef MODEM_H
 #define MODEM_H
 
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
+#include "modem.hpp"
 
-// Quelltext fuer Windows
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-/*
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
 
-#include <qobject.h>
-#include <windows.h>
-#include <QString>
+// // Quelltext fuer Windows
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// /*
 
-//#include <qdir.h>
+// #include <qobject.h>
+// #include <windows.h>
+// #include <QString>
 
-//#include <sys/types.h>
-//#include <termios.h>
-//#include <unistd.h>
+// //#include <qdir.h>
 
-//#include <qsocketnotifier.h>
+// //#include <sys/types.h>
+// //#include <termios.h>
+// //#include <unistd.h>
 
-//#include <config.h>
+// //#include <qsocketnotifier.h>
 
-//modem class for serial communication
-//  *@author Lars Schnake
-//  *@author DL4JAL Andreas Lindenau Aenderungen fuer NWT7 + HFM9
-//
+// //#include <config.h>
 
-enum wBaud{
-  wBaud_9600=9600,
-  wBaud_57600=57600,
-  wBaud_115200=115200
-};
+// //modem class for serial communication
+// //  *@author Lars Schnake
+// //  *@author DL4JAL Andreas Lindenau Aenderungen fuer NWT7 + HFM9
+// //
 
-enum wParity{
-  wPARITY_NONE,
-  wPARITY_ODD,
-  wPARITY_EVEN,
-  wPARITY_MARK,
-  wPARITY_SPACE
-};
+// enum wBaud{
+//   wBaud_9600=9600,
+//   wBaud_57600=57600,
+//   wBaud_115200=115200
+// };
 
-enum wSerialLineState{
-  wSERIAL_LINESTATE_DCD = 0x040,
-  wSERIAL_LINESTATE_CTS = 0x020,
-  wSERIAL_LINESTATE_DSR = 0x100,
-  wSERIAL_LINESTATE_DTR = 0x002,
-  wSERIAL_LINESTATE_RING = 0x080,
-  wSERIAL_LINESTATE_RTS = 0x004,
-  wSERIAL_LINESTATE_NULL = 0x000,
-};
+// enum wParity{
+//   wPARITY_NONE,
+//   wPARITY_ODD,
+//   wPARITY_EVEN,
+//   wPARITY_MARK,
+//   wPARITY_SPACE
+// };
 
-struct SerialPort_DCS{
-  wBaud baud;
-  wParity parity;
-  unsigned char wordlen;
-  unsigned char stopbits;
-  bool rtscts;
-  bool xonxoff;
-  SerialPort_DCS(){
-    baud = wBaud_57600;
-    parity = wPARITY_NONE;
-    stopbits = 1;
-    rtscts = false;
-    xonxoff = false;
-  };
-};
+// enum wSerialLineState{
+//   wSERIAL_LINESTATE_DCD = 0x040,
+//   wSERIAL_LINESTATE_CTS = 0x020,
+//   wSERIAL_LINESTATE_DSR = 0x100,
+//   wSERIAL_LINESTATE_DTR = 0x002,
+//   wSERIAL_LINESTATE_RING = 0x080,
+//   wSERIAL_LINESTATE_RTS = 0x004,
+//   wSERIAL_LINESTATE_NULL = 0x000,
+// };
 
-struct SerialPort_EINFO{
-  int brk;
-  int frame;
-  int overrun;
-  int parity;
-  SerialPort_EINFO(){
-    brk = frame = overrun = parity = 0;
-  };
-};
+// struct SerialPort_DCS{
+//   wBaud baud;
+//   wParity parity;
+//   unsigned char wordlen;
+//   unsigned char stopbits;
+//   bool rtscts;
+//   bool xonxoff;
+//   SerialPort_DCS(){
+//     baud = wBaud_57600;
+//     parity = wPARITY_NONE;
+//     stopbits = 1;
+//     rtscts = false;
+//     xonxoff = false;
+//   };
+// };
 
-class Modem : public QObject  {
-Q_OBJECT
-public:
-  Modem();
-  ~Modem();
+// struct SerialPort_EINFO{
+//   int brk;
+//   int frame;
+//   int overrun;
+//   int parity;
+//   SerialPort_EINFO(){
+//     brk = frame = overrun = parity = 0;
+//   };
+// };
 
-// lock and open serial port with settings of ConfigData
-  bool opentty(QString seriell_dev);
-// unlock and close serial port
-  bool closetty();
-// write a char to port
-  bool writeChar(unsigned char);
-// write a char[] to port
-  bool writeLine(const char *);
-// returns the last modem (error?)message 
-//  const QString modemMessage();
-// disconnect the notify connection ( see notify )
-//  void stop();
-// simple read from port 
-//  int rs232_read(void *, int);
-//  int getFD();
-// neue funktion fuer den NWT7
-  int readttybuffer(void *, int);
-  static Modem *modem;
+// class Modem : public QObject  {
+// Q_OBJECT
+// public:
+//   Modem();
+//   ~Modem();
 
-  HANDLE fd;
-  OVERLAPPED ov;
-  SerialPort_DCS dcs;
-  SerialPort_EINFO einfo;
+// // lock and open serial port with settings of ConfigData
+//   bool opentty(QString seriell_dev);
+// // unlock and close serial port
+//   bool closetty();
+// // write a char to port
+//   bool writeChar(unsigned char);
+// // write a char[] to port
+//   bool writeLine(const char *);
+// // returns the last modem (error?)message 
+// //  const QString modemMessage();
+// // disconnect the notify connection ( see notify )
+// //  void stop();
+// // simple read from port 
+// //  int rs232_read(void *, int);
+// //  int getFD();
+// // neue funktion fuer den NWT7
+//   int readttybuffer(void *, int);
+//   static Modem *modem;
 
-  int CloseDevice();
-  int OpenDevice(const char* devname, void* dcs);
+//   HANDLE fd;
+//   OVERLAPPED ov;
+//   SerialPort_DCS dcs;
+//   SerialPort_EINFO einfo;
 
-signals:
-// is emitted if data was received
-//  void charWaiting(unsigned char);
-  void setTtyText(QString); 
+//   int CloseDevice();
+//   int OpenDevice(const char* devname, void* dcs);
 
-public slots:
+// signals:
+// // is emitted if data was received
+// //  void charWaiting(unsigned char);
+//   void setTtyText(QString); 
 
-	// notify if chars arriving from ptc
-//  void startNotifier();
-	// stop the notifier
-//  void stopNotifier();
-	// read from port
-//  void readtty(int);
+// public slots:
 
-private:
-//  speed_t modemspeed();  // parse modem speed
-  bool modem_is_locked;
-  int modemfd;
-//  QSocketNotifier *sn;
-  bool data_mode;
-  QString errmsg;
-//  QString seriell_dev;
-//  struct termios initial_tty;
-//  struct termios tty;
-  unsigned char dataMask;
-//  bool lock_device();
-//  bool unlock_device();
-  QString qdev;
-};
-*/
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-// Quelltext fuer Linux
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-//#############################################################################
-#include <qobject.h>
+// 	// notify if chars arriving from ptc
+// //  void startNotifier();
+// 	// stop the notifier
+// //  void stopNotifier();
+// 	// read from port
+// //  void readtty(int);
 
-#include <qdir.h>
+// private:
+// //  speed_t modemspeed();  // parse modem speed
+//   bool modem_is_locked;
+//   int modemfd;
+// //  QSocketNotifier *sn;
+//   bool data_mode;
+//   QString errmsg;
+// //  QString seriell_dev;
+// //  struct termios initial_tty;
+// //  struct termios tty;
+//   unsigned char dataMask;
+// //  bool lock_device();
+// //  bool unlock_device();
+//   QString qdev;
+// };
+// */
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// // Quelltext fuer Linux
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// //#############################################################################
+// #include <qobject.h>
 
-#include <sys/types.h>
-//#include <termios.h>
-#include <unistd.h>
+// #include <qdir.h>
 
-#include <qsocketnotifier.h>
+// #include <sys/types.h>
+// //#include <termios.h>
+// #include <unistd.h>
 
-#include "constdef.h"
+// #include <qsocketnotifier.h>
 
-//#include <config.h>
+// #include "constdef.h"
 
-//modem class for serial communication
-//  *@author Lars Schnake
-//  *@author DL4JAL Andreas Lindenau Aenderungen fuer NWT7 + HFM9 + NWT FA
+// //#include <config.h>
 
-class Modem : public QObject  {
+// //modem class for serial communication
+// //  *@author Lars Schnake
+// //  *@author DL4JAL Andreas Lindenau Aenderungen fuer NWT7 + HFM9 + NWT FA
 
-Q_OBJECT
-public:
-  Modem();
-  ~Modem();
+// class Modem : public QObject  {
 
-// lock and open serial port with settings of ConfigData
-  bool opentty(QString seriell_dev);
-// unlock and close serial port
-  bool closetty();
-// write a char to port
-  bool writeChar(unsigned char);
-// write a char[] to port
-  bool writeLine(const char *);
-// returns the last modem (error?)message
-  const QString modemMessage();
-// disconnect the notify connection ( see notify )
-  void stop();
-// simple read from port
-  int rs232_read(void *, int);
-  int getFD();
-// neue funktion fuer den NWT7
-  int readttybuffer(void *, int);
+// Q_OBJECT
+// public:
+//   Modem();
+//   ~Modem();
+
+// // lock and open serial port with settings of ConfigData
+//   bool opentty(QString seriell_dev);
+// // unlock and close serial port
+//   bool closetty();
+// // write a char to port
+//   bool writeChar(unsigned char);
+// // write a char[] to port
+//   bool writeLine(const char *);
+// // returns the last modem (error?)message
+//   const QString modemMessage();
+// // disconnect the notify connection ( see notify )
+//   void stop();
+// // simple read from port
+//   int rs232_read(void *, int);
+//   int getFD();
+// // neue funktion fuer den NWT7
+//   int readttybuffer(void *, int);
 
 
-public:
-  static Modem *modem;
+// public:
+//   static Modem *modem;
 
-signals:
-// is emitted if data was received
-  void charWaiting(unsigned char);
-  void setTtyText(QString);
+// signals:
+// // is emitted if data was received
+//   void charWaiting(unsigned char);
+//   void setTtyText(QString);
 
-public slots:
+// public slots:
 
-	// notify if chars arriving from ptc
-  void startNotifier();
-	// stop the notifier
-  void stopNotifier();
-	// read from port
-  void readtty(int);
+// 	// notify if chars arriving from ptc
+//   void startNotifier();
+// 	// stop the notifier
+//   void stopNotifier();
+// 	// read from port
+//   void readtty(int);
 
-private:
-  speed_t modemspeed();  // parse modem speed
-  bool modem_is_locked;
-  int modemfd;
-  QSocketNotifier *sn;
-  bool data_mode;
-  QString errmsg;
-//  QString seriell_dev;
-  struct termios initial_tty;
-  struct termios tty;
-  unsigned char dataMask;
-  bool lock_device();
-  bool unlock_device();
-  QString qdev;
-};
+// private:
+//   speed_t modemspeed();  // parse modem speed
+//   bool modem_is_locked;
+//   int modemfd;
+//   QSocketNotifier *sn;
+//   bool data_mode;
+//   QString errmsg;
+// //  QString seriell_dev;
+//   struct termios initial_tty;
+//   struct termios tty;
+//   unsigned char dataMask;
+//   bool lock_device();
+//   bool unlock_device();
+//   QString qdev;
+// };
 
 #endif
 
